78cae9db3ad615e4a7c7c48200fcba612a2e6907
Seryozha Poghosyan <seryozhapoghosyan@workfront.com>
2016-10-06 20:24:56 +0400

    Created Xcode project.

---
+++ NarinjDataModelExamples/NarinjDataModelExamples/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NarinjDataModelExamples
+//
+//  Created by Seryozha Poghosyan on 10/6/16.
+//  Copyright © 2016 ACA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NarinjDataModelExamples/NarinjDataModelExamples/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NarinjDataModelExamples
+//
+//  Created by Seryozha Poghosyan on 10/6/16.
+//  Copyright © 2016 ACA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
